029a70d654a287bf31e1947940e74d80d8b79367
Sindisiwe Zinhle Kubheka <SindisiweK@SAHOMELOANS.COM>
2018-06-25 16:06:03 +0200

    Aspose pdf refactored sltn

---
+++ PdfForm/PdfForm/AsposePdfFormProcessor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Aspose.Pdf.Cloud.Sdk.Api;
+using Aspose.Pdf.Cloud.Sdk.Model;
+using Aspose.Storage.Cloud.Sdk.Api;
+using Aspose.Storage.Cloud.Sdk.Model;
+using Aspose.Storage.Cloud.Sdk.Model.Requests;
+using iTextSharp.text.pdf;
+using RestSharp.Extensions;
+
+namespace PdfForm
+{
+    public  class AsposePdfFormProcessor
+    {
+        public readonly string _apiKey;
+        public readonly string _appSid;
+
+      
+        public AsposePdfFormProcessor()
+        {
+            _apiKey = ConfigurationManager.AppSettings["APIKEY"];
+            _appSid = ConfigurationManager.AppSettings["APPSID"];
+        }
+        public UploadResponse UploadPdf(string pdfPath, string fileName)
+        {
+            using (var stream = new FileStream(pdfPath, FileMode.Open))
+            {
+                var storageApi = new StorageApi(_apiKey, _appSid);
+                var request = new PutCreateRequest(fileName, stream);
+                var actual = storageApi.PutCreate(request);
+                return actual;
+            }
+        }
+        public FieldResponse UpdateSpecificField(string fileName, string fieldname, Field body)
+        {
+            var target = new PdfApi(_apiKey, _appSid);
+            var apiResponse = target.PutUpdateField(fileName, fieldname, body);
+            return apiResponse;
+        }
+
+        public   FieldsResponse UpdateMultipleValues(string fileName, Fields body)
+        {
+            var target = new PdfApi(_apiKey, _appSid);
+            var apiResponse = target.PutUpdateFields(fileName, body);
+            return apiResponse;
+        }
+
+        public  PdfStamper DisableFormFields(PdfReader reader, string newFile, List<string> ListOfFields)
+        {
+            using (var stamper = new PdfStamper(reader, new FileStream(newFile, FileMode.Create)))
+            {
+                var fields = stamper.AcroFields;
+
+                SetReadOnly(fields,ListOfFields);
+
+                stamper.Close();
+                return stamper;
+            }
+        }
+
+        public string UpdatePdf(string fileName)
+        {
+            string message = "";
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                message = "Page Not Found";
+            }
+                return message;
+        }
+        public byte[] DownloadPdf(string pdfPath, string fileName)
+        {
+            dynamic bytesOfDownloadedPdf ;
+            var storageApi = new StorageApi(_apiKey, _appSid);
+            var request = new GetDownloadRequest(fileName);
+            using (var response = storageApi.GetDownload(request))
+            {
+                var bytes = response.ReadAsBytes();
+                File.WriteAllBytes(pdfPath,bytes);
+                bytesOfDownloadedPdf  = File.ReadAllBytes(pdfPath);
+            }
+
+            return bytesOfDownloadedPdf ;
+        }
+
+        public RemoveFileResponse DeletePdf(string fileName)
+        {
+            
+            var storageApi = new StorageApi(_apiKey, _appSid);
+            var request = new DeleteFileRequest(fileName);
+            var apiResponse = storageApi.DeleteFile(request);
+
+            return apiResponse;
+        }
+        private static void SetReadOnly(AcroFields fields, IEnumerable<string> ListOfFields)
+        {
+            foreach (var field in ListOfFields)
+            {
+                fields.SetFieldProperty(field, "setfflags", PdfFormField.FF_READ_ONLY, null);
+            }
+        }
+    }
+}
---
+++ PdfForm/PdfForm/AsposePdfTest.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Storage.Cloud.Sdk.Api;
+using Aspose.Pdf;
+using Aspose.Pdf.Cloud.Sdk.Client;
+using Aspose.Pdf.Cloud.Sdk.Model;
+using Aspose.Storage.Cloud.Sdk.Model.Requests;
+using NUnit.Framework;
+using iTextSharp.text.pdf;
+using Aspose.Pdf.Cloud.Sdk.Api;
+using Aspose.Storage.Cloud.Sdk.Model;
+
+namespace PdfForm
+{
+    [TestFixture]
+    public class AsposePdfTest
+    {
+      
+        [Test]
+        public void UploadFormToCloud_GivenFileNameAndPath_ShouldUploadAPdfDocumentToCloud()
+        {
+            //Arrange
+            var fileName = "EditableForm1.pdf";
+            var baseDirectory = TestContext.CurrentContext.TestDirectory;
+            var pdfPath = Path.Combine(baseDirectory, fileName);
+
+            var sut = CreateAsposePdfForm();
+            //Act
+            var actual =sut.UploadPdf(pdfPath,fileName);
+
+            //Assert
+            Assert.AreEqual(200, actual.Code);
+        }
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void GetAFile_GivenInvalidFileName_ShouldReturnErrormessageOfPdfNotFound(string fileName)
+        {
+            //Arrange
+            var asposePdfForm = CreateAsposePdfForm();
+            //Act
+            var actual = asposePdfForm.UpdatePdf(fileName);
+
+            //Assert
+            Assert.AreEqual("Page Not Found", actual);
+        }
+        [Test]
+        public void UpdateSpecificField_GivenFirstNameField_ShouldUpdatePdfWithNewFieldValue()
+        {
+
+            //Arrange
+            string fileName = "EditableForm1.pdf";
+            var fieldname = "FirstName";
+
+            var asposePdfForm = CreateAsposePdfForm();
+            Field body = new Field
+            {
+                Name = "FirstName",
+                Values = new List<string> { "Sindy" }
+            };
+
+            //Act
+            var actual = asposePdfForm.UpdateSpecificField(fileName, fieldname,body);
+
+            //Assert
+            Assert.AreEqual("OK",actual.Status);
+        }
+       
+        [Test]
+        public void UpdateFormPdf_GivenMultipleFieldValues_ShouldUpdatePdfWithNewValues()
+        {
+            //Arrange
+            var asposePdfForm = CreateAsposePdfForm();
+            var fileName = "EditableForm2.pdf";
+            Fields body = GetMultipleValuesToBeUpdated();
+
+            //Act
+            var actual = asposePdfForm.UpdateMultipleValues(fileName, body);
+
+            //Assert
+            Assert.AreEqual("OK", actual.Status);
+        }
+      
+        [Test]
+        public void DisableFormFields_GivenValuesToDisable_ShouldAvoidValuesFromBeingEditable()
+        {
+          
+            //Arrange
+            var formFile ="EditableForm2.pdf";
+            var newFile = "NewEditableForm.pdf";
+            var baseDirectory = TestContext.CurrentContext.TestDirectory;
+            var oldPath = Path.Combine(baseDirectory, formFile);
+            var newPath = Path.Combine(baseDirectory, newFile);
+
+            var reader = new PdfReader(oldPath);
+
+            var sut = CreateAsposePdfForm();
+            var fieldToSetToReadOnly = new List<string>{ "FirstName","Surname","DateOfBirth" };
+
+            //Act
+            sut.DisableFormFields(reader, newPath, fieldToSetToReadOnly);
+            //Assert
+            Assert.AreNotSame(formFile,newFile);
+
+        }
+
+        [Test]
+        public void DownloadPdf_GivenFileNameAndPath_ShouldDownloadExistingPdfFromCloud()
+        {
+            //Arrange
+            var fileName = "EditableForm1.pdf";
+            var baseDirectory = TestContext.CurrentContext.TestDirectory;
+            var path = Path.Combine(baseDirectory, fileName);
+
+            var asposePdfForm = CreateAsposePdfForm();
+            //Act
+            var actual = asposePdfForm.DownloadPdf(path, fileName);
+
+            //Assert
+            Assert.AreEqual(6179728, actual.Length);
+
+        }
+
+        [Test]
+        public void DeletePdf_GivenADirectory_ShouldReturnAvailableDocuments()
+        {
+            //Arrange
+            var fileName = "EditableForm1.pdf";
+            
+            var asposePdfForm = CreateAsposePdfForm();
+            //Act
+            var actual = asposePdfForm.DeletePdf(fileName);
+
+            //Assert
+            Assert.AreEqual("OK",actual.Status);
+        }
+       
+        public AsposePdfFormProcessor CreateAsposePdfForm()
+        {
+            return new AsposePdfFormProcessor();
+        }
+        private static Fields GetMultipleValuesToBeUpdated()
+        {
+            return new Fields()
+            {
+                List = new List<Field>
+                {
+                    new Field()
+                    {
+                        Name = "FirstName",
+                        Values = new List<string> {"Zinhle"}
+                    },
+                    new Field()
+                    {
+                        Name = "Surname",
+                        Values = new List<string> {"Pantshwa"}
+                    },
+                    new Field()
+                    {
+                        Name = "DateOfBirth",
+                        Values = new List<string> {"1998-10-04"}
+                    },
+                    new Field()
+                    {
+                        Name = "GrossSalary",
+                        Values = new List<string> {"25000"}
+                    },
+                    new Field()
+                    {
+                        Name = "Tax",
+                        Values = new List<string> {"3000"}
+                    },
+                    new Field()
+                    {
+                        Name = "Accomodation",
+                        Values = new List<string> {"4000"}
+                    },
+                    new Field()
+                    {
+                        Name = "CellPhone",
+                        Values = new List<string> {"350"}
+                    },
+                    new Field()
+                    {
+                        Name = "CreditCard",
+                        Values = new List<string> {"2600"}
+                    },
+                    new Field()
+                    {
+                        Name = "OtherDept",
+                        Values = new List<string> {"2100"}
+                    }
+                }
+            };
+        }
+    }
+}
